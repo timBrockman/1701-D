afc838562b8146fadd32a7991cd27571f389a1cc
timBrockman <tim@timbrockman.com>
2015-10-22 23:25:35 -0400

    man: grunt connect

---
+++ gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect:{server:{options:{
+      port:8080,
+      keepalive:true
+    }}}
+  });
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('default','connect');
+}
